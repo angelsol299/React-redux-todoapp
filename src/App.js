34f2629876dcac619e4a273b894e7754acef4f48
angelsol299 <32056957+angelsol299@users.noreply.github.com>
2018-10-31 21:48:37 +0100

    navbar
    navbar code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Todos from "./Todos";
 import AddTodo from "./AddTodo";
+import Navbar from "./Navbar";
 
 class App extends Component {
   state = {
@@ -27,7 +28,8 @@ class App extends Component {
   render() {
     return (
       <div className="todo-app container">
-        <h1 className="center blue-text">Todo's</h1>
+        <Navbar />
+        <h1 className="center green-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
         <AddTodo addTodo={this.addTodo} />
       </div>
