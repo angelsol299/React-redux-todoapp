50e44ef1524173d5433a995eb489059e76823bda
angelsol299 <32056957+angelsol299@users.noreply.github.com>
2018-10-31 21:06:07 +0100

    main file of react redux todo
    files for the app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Todos from "./Todos";
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "get veggies" },
+      { id: 2, content: "learn to code" }
+    ]
+  };
+  deleteTodo = id => {
+    console.log(id);
+  };
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
+}
+
+export default App;
