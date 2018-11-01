cdc795ea4bf4d23d925954283ba823c2c25e6b37
angelsol299 <32056957+angelsol299@users.noreply.github.com>
2018-11-01 18:30:13 +0100

    change name
    logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
         <nav>
           <div class="nav-wrapper green">
             <a href="#home" class="brand-logo">
-              Logo
+              Todos Apps
             </a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
               <li>
